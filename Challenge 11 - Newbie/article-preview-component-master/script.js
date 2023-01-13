9e2513dbc5ab044fca597944d8be08b8a31a57cb
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2023-01-13 19:05:07 -0300

    Challenge 11 mobile version finished

--- Challenge 11 - Newbie/article-preview-component-master/script.js
+++ Challenge 11 - Newbie/article-preview-component-master/script.js
@@ -1,6 +1,7 @@
-const active = document.querySelector(".container-share");
+const none = document.querySelector(".container-share");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  active.classList.toggle("none");
+  none.classList.toggle("none");
+  btn.classList.toggle("active");
 });
