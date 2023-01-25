64146a5d8a146c9ee74efd74a72d59113eb2565c
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2023-01-25 16:40:08 -0300

    Challenge 14 animation finished

--- Challenge 14 - Newbie/intro-component-with-signup-form-master/script.js
+++ Challenge 14 - Newbie/intro-component-with-signup-form-master/script.js
@@ -4,7 +4,7 @@ let flagPoles = CSSRulePlugin.getRule(".container-input");
 gsap.fromTo(
   ".loading-page",
   { opacity: 1 },
-  { opacity: 0, duration: 1.5, delay: 2 }
+  { opacity: 0, duration: 1.5, delay: 2, display: "none" }
 );
 
 tl.fromTo(
